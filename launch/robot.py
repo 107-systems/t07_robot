e5d5ff64d2f01e0b646700232d37c955130976e3
Alexander Entinger <consulting@lxrobotics.com>
2023-11-15 18:40:00 +0100

    Increase deadline to 200 milliseconds, since publish rate is 100 milliseconds.

--- launch/robot.py
+++ launch/robot.py
@@ -14,11 +14,11 @@ def generate_launch_description():
         {'can_iface' : 'can0'},
         {'can_node_id' : 100},
         {'motor_left_topic': '/motor/left/target'},
-        {'motor_left_topic_deadline_ms': 100},
+        {'motor_left_topic_deadline_ms': 200},
         {'motor_left_topic_liveliness_lease_duration': 1000},
         {'motor_left_pwm_port_id': 100},
         {'motor_right_topic': '/motor/right/target'},
-        {'motor_right_topic_deadline_ms': 100},
+        {'motor_right_topic_deadline_ms': 200},
         {'motor_right_topic_liveliness_lease_duration': 1000},
         {'motor_right_pwm_port_id': 101},
       ]
